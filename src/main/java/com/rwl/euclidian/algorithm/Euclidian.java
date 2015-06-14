360d0b4a6ca656d1bd3991b42f0a248dc1feaea7
RainWhileLoop <RainWhileLoop@Rainy-PC>
2015-06-14 16:26:41 +0700

    edit the initial condition

--- src/main/java/com/rwl/euclidian/algorithm/Euclidian.java
+++ src/main/java/com/rwl/euclidian/algorithm/Euclidian.java
@@ -13,23 +13,31 @@ public class Euclidian {
 
     /**
      * Calculated the GCD of 2 numbers by using Euclidian Algorithm
+     *
      * @param num1 must not be 0
      * @param num2 must not be 0
      * @return GCD - greatest common divisor
      */
     public static int findGCD(int num1, int num2) {
-        if (num1 == 0 || num2 == 0) {
-            throw new ArithmeticException("Number must not zero!!!");
+        if (num1 == 0) {
+            return num2;
         }
+        if (num2 == 0) {
+            return num1;
+        }
+
         int dividend = Math.max(num1, num2);
         int divisor = Math.min(num1, num2);
 
         int fraction = dividend % divisor;
-        
+
         if (fraction != 0) {
             return findGCD(divisor, fraction);
         }
         return divisor;
     }
 
+    private static void showProcess(int max, int min) {
+        System.out.println(max + " = " + min + "(" + max / min + ") + " + max % min);
+    }
 }
